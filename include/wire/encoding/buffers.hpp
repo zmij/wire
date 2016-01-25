a66a1dc41ff2d740e51afb556e2b6476427199d8
zmij <sergei.a.fedorov@gmail.com>
2016-01-25 12:34:32 +0300

    Move read/write guts to a detail header

--- include/wire/encoding/buffers.hpp
+++ include/wire/encoding/buffers.hpp
@@ -10,68 +10,13 @@
 
 #include <type_traits>
 #include <iterator>
-#include <wire/encoding/detail/wire_traits.hpp>
-#include <wire/encoding/detail/helpers.hpp>
-#include <wire/encoding/detail/fixed_io.hpp>
-#include <wire/encoding/detail/varint_io.hpp>
-#include <wire/encoding/detail/string_io.hpp>
+#include <wire/encoding/detail/wire_io.hpp>
 
 #include <bitset>
 #include <iostream>
 
 namespace wire {
 namespace encoding {
-namespace detail {
-
-template < typename T, wire_types >
-struct writer_impl;
-template < typename T, wire_types >
-struct reader_impl;
-
-template < typename T >
-struct writer_impl< T, SCALAR_FIXED >
-	: fixed_size_writer< T > {};
-template < typename T >
-struct reader_impl< T, SCALAR_FIXED >
-	: fixed_size_reader< T > {};
-
-template < typename T >
-struct writer_impl<T, SCALAR_VARINT >
-	: varint_writer< T, std::is_signed< typename std::decay<T>::type >::value > {};
-template < typename T >
-struct reader_impl< T, SCALAR_VARINT >
-	: varint_reader< T, std::is_signed< typename std::decay<T>::type >::value > {};
-
-template <>
-struct writer_impl< std::string, SCALAR_WITH_SIZE >
-	: string_writer {};
-template <>
-struct reader_impl< std::string, SCALAR_WITH_SIZE >
-	: string_reader {};
-
-template < typename T >
-struct writer : writer_impl< T, wire_type<T>::value> {};
-
-template < typename T >
-struct reader : reader_impl< T, wire_type<T>::value> {};
-
-template < typename OutputIterator, typename T >
-void
-write(OutputIterator o, T v)
-{
-	typedef writer< typename std::decay< T >::type > writer_type;
-	writer_type::write(o, v);
-}
-
-template < typename InputIterator, typename T >
-void
-read(InputIterator& begin, InputIterator end, T& value)
-{
-	typedef reader< typename std::decay< T >::type > reader_type;
-	reader_type::read(begin, end, value);
-}
-
-}  // namespace detail
 
 template < typename OutputIterator >
 class outgoing {
