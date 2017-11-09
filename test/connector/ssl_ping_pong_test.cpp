4a9ed3616a38313037dd2977ad3a8509ecb28937
zmij <sergei.a.fedorov@gmail.com>
2017-11-09 11:19:29 +0300

    Fix typo

--- test/connector/ssl_ping_pong_test.cpp
+++ test/connector/ssl_ping_pong_test.cpp
@@ -83,7 +83,7 @@ TEST_F(SSLPingPong, SyncRoundtrip)
     SyncRoundtrip();
 }
 
-TEST_F(SSLPingPong, DISBLED_MTConnectionUsage)
+TEST_F(SSLPingPong, DISABLED_MTConnectionUsage)
 {
     MTConnectionUsage();
 }
